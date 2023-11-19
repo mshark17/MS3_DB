df452ad25b5c1781915bd8d3985fba2360aa905c
mshark17 <mostafasharkawy17@aucegypt.edu>
2023-11-19 20:01:34 +0200

    Update Gui.py

--- Gui.py
+++ Gui.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import mysql.connector
 import pandas as pd
-import datetime
-import re
-
-pattern = r'\d{5}-\d{5}-\d{5}'
 
 def connect_to_database():  
     mydb = mysql.connector.connect(
@@ -26,22 +22,11 @@ def register_user(mydb):
     temp = st.text_input("Enter list of skills (seperate by comma and space): Skill1, Skill2")
     listofskills = temp.split(", ")
     
-    pattern = r'\d{5}-\d{5}-\d{5}'
-
-    match = re.match(pattern, dateofbirth)
-
-
-    if(match):
-        dob_date = datetime.datetime.strptime(dateofbirth, '%Y-%m-%d')
-
-        today = datetime.date.today()
-        age = today.year - dob_date.year - ((today.month, today.day) < (dob_date.month, dob_date.day))
-    
     try:
         if st.button("Submit"):
             
             query= """
-                INSERT INTO user VALUES (\'"""+email+"""\', \'"""+dateofbirth+ """\', \'"""+username+"""\', \'"""+gender+"""\', \'"""+gpa+"""\', \'"""+listofskills+"""\')
+                INSERT INTO user VALUES (\'"""+email+"""\', \'"""+username+"""\', \'"""+gender+ """\', \'"""+dateofbirth+"""\',\'"""+gpa+"""\', \'"""+name+"""\')
                 """
             
             try:
@@ -49,6 +34,18 @@ def register_user(mydb):
                 mycursor.execute(query)
                 mydb.commit()
                 st.subheader('User Added Succesfully!')
+            except:
+                st.subheader('ERROR')
+
+            query= """
+                INSERT INTO user_list_of_skills VALUES (\'"""+listofskills+"""\',\'"""+username+"""\')
+                """
+            
+            try:
+                mycursor = mydb.cursor()
+                mycursor.execute(query)
+                mydb.commit()
+                st.subheader('User_list_of_skills Added Succesfully!')
             except:
                 st.subheader('ERROR')
     except:
