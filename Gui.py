8f166964b480da1b6efff9dad9df952b744d0614
mshark17 <mostafasharkawy17@aucegypt.edu>
2023-11-20 01:03:59 +0200

    Update Gui.py

--- Gui.py
+++ Gui.py
@@ -64,6 +64,7 @@ def appyForJob(database):
         st.write(output)
         st.subheader("In Salary, '-1' means Confidential")
         jobID=st.text_input("Enter ID of the Job you want to apply to: Ex. 10011001")
+        username=st.text_input("Enter your username:")
         cv=st.file_uploader("Upload CV or Resume")
         if st.button("Submit"):
             st.subheader("You applied to Job:{}".format(jobID))
